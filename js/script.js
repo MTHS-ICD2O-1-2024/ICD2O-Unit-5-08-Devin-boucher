2c31139efc61b56deca6c94a5a0f80e45dbd4028
default <devin.boucher@stu.ocsb.ca>
2025-05-08 16:50:31 +0000

    made code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
 // Created by: Devin Boucher
 // Created on: April 2025
-// This file contains the JS functions for index.html
+// This file contains the JS logic for simplified long division
 
 'use strict'
 
-// eslint-disable-next-line no-unused-vars
-function sumNumbers () {
-  const userInput = parseInt(document.getElementById('user-number').value)
-  let sum = 0
+function longDivide () {
+  let dividend = parseInt(document.getElementById('dividend').value)
+  let divisor = parseInt(document.getElementById('divisor').value)
+  let quotient = 0
+  let steps = ''
 
-  for (let counter = 1; counter <= userInput; counter++) {
-    sum += counter
+  // Loop to subtract divisor from dividend
+  while (true) {
+    if (dividend >= divisor) {
+      steps += `${dividend} - ${divisor} = ${dividend - divisor}<br>`
+      dividend -= divisor
+      quotient++
+    } else {
+      break
+    }
   }
 
-  document.getElementById('sum-result').innerHTML = `<p><strong>The sum of all numbers from 1 to ${userInput} is ${sum}.</strong></p>`
+  document.getElementById('sum-result').innerHTML =
+    `<p><strong>Steps:</strong><br>${steps}</p>
+    <p><strong>Result:</strong> Quotient = ${quotient}, Remainder = ${dividend}</p>`
 }
