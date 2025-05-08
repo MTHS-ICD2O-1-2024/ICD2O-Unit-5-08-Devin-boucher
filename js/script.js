49f6e3cc4200d94ee62891ab5b2fa16d1df7728c
default <devin.boucher@stu.ocsb.ca>
2025-05-08 16:38:49 +0000

    added old code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Devin Boucher
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+// eslint-disable-next-line no-unused-vars
+function sumNumbers () {
+  const userInput = parseInt(document.getElementById('user-number').value)
+  let sum = 0
+
+  for (let counter = 1; counter <= userInput; counter++) {
+    sum += counter
+  }
+
+  document.getElementById('sum-result').innerHTML = `<p><strong>The sum of all numbers from 1 to ${userInput} is ${sum}.</strong></p>`
+}
